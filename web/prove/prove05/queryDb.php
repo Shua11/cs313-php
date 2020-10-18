a592eaf09089af452596812261e1372188d844d7
Shua11 <math.joshua11@gmail.com>
2020-10-18 01:43:39 -0400

    Adding/updating db files and prove05 files

--- web/prove/prove05/queryDb.php
+++ web/prove/prove05/queryDb.php
@@ -14,17 +14,17 @@
       $project_image_description = $pRow["project_image_description"];
       $bFeatured = $pRow["bFeatured"];
 
-      // echo '$project_name';
+      echo '$pRow';
 
       // if ($project_bFeatured == true)
       // {
          echo '<div class="card border-dark shadow-lg text-center" data-aos="fade-in" data-aos-duration="1500" data-aos-delay="100">
-                  <img src="$project_image" class="card-img-top" alt="$project_image_description">
+                  <img src="'$project_image'" class="card-img-top" alt="'$project_image_description'">
                   <div class="card-body">
-                     <h5>"$project_name"</h5>
+                     <h5>"'$project_name'"</h5>
                   </div>
                   <div class="card-footer">
-                     <p>"$project_note"</p>
+                     <p>"'$project_note'"</p>
                      <!-- <a href="#" class="btn btn-primary w-100">View Project</a> -->
                      <button type="button" class="btn btn-primary w-100" data-toggle="modal" data-target="#Modal2">
                         View Project
