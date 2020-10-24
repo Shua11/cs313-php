f3354b8bab3d45c82e2a39df9d14012b0e93ca62
Shua11 <math.joshua11@gmail.com>
2020-10-24 16:35:54 -0400

    testing file uploads

--- db/myInsert.sql
+++ db/myInsert.sql
@@ -19,7 +19,7 @@ INSERT INTO project  ( project_name
               VALUES ( 'Westside Ave. Action Plan (WAAP)- West Colorado Ave. Design of Water and Sanitary Sewer Replacement'
                      , 'PRC enjoied working with FHU, CH2M Hill and Wildcat Construction!'
                      , 'PRC was responsible for the design of the Water Plans and Sanitary Sewer Replacement. PRC worked closely with Town of Manitou, Colorado Springs Utilities, El Paso County and Felsburg Holt & Ullevig (FHU), to prepare Preliminary and Final Construction Documents. The improvements included replacement of 16-inch and 30-inch waterlines as well as 12-inch sanitary sewer line replacement and sewer line rehabilitation (lining). Many of the existing utilities are very old and were not documented or accurately mapped when originally constructed. As such, PRC worked very closely with Wildcat Construction throughout the duration of the project to address unforeseen issues and to adjust designs when necessary.'
-                     , 'images/Academy-Bridge2-791x1024.jpg'
+                     , 'images/W-Colorado-1024x721.jpg'
                      , 'W-Colorado Image'
                      , true);
 INSERT INTO project  ( project_name
@@ -31,6 +31,6 @@ INSERT INTO project  ( project_name
               VALUES ( 'Private Residence- 12 Random Rd. Pond maintenance and Bank Stabilization'
                      , 'PRCs experience and relationships ensured another successful project!'
                      , 'PRC was engaged by the homeowner to assist with a Floodplain Development Permit application. PRC worked with Bailey Professional Solutions (BPS) to conduct a survey of the existing conditions. PRC then developed plans and specifications for bank stabilization and the dredging of the existing pond. PRC prepared the applications required by Cherry Hills Village, the State Engineers Office and the United States Army Corps of Engineers. PRC has great working relationships with these agencies and were able to meet with them and discuss the implications of proposed improvements. After successful approvals from all stakeholders, the project was completed. PRC also provided Construction Administrative Services until completion of the landscape installation.'
-                     , 'images/Academy-Bridge2-791x1024.jpg'
+                     , 'images/Random-Rd-pond2-791x1024.jpg'
                      , 'Random Rd Pond Image'
                      , true);
