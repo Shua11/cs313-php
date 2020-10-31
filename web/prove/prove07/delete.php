e82596d81521be6937c467009d51e20bdbb61148
Shua11 <math.joshua11@gmail.com>
2020-10-31 17:56:35 -0400

    prove07 wip

--- web/prove/prove07/delete.php
+++ web/prove/prove07/delete.php
@@ -1,29 +1,23 @@
 <?php
 session_start();
 
-$selected = $_POST['inputSelect'];
-   
-echo $selected;
-
 if (isset($_POST['inputSelect']))
 {
    $selected = $_POST['inputSelect'];
-   
-   echo ' isset';
 
     // Connect to the DB
-	// require "dbConnect.php";
-	// $db = get_db();
-	// $query = 'DELETE FROM project WHERE project_name=:selected';
+	require "dbConnect.php";
+	$db = get_db();
+	$query = 'DELETE FROM project WHERE id=:selected';
 
-	// $statement = $db->prepare($query);
-	// $statement->bindValue(':selected', $selected);
-	// $result = $statement->execute();
+	$statement = $db->prepare($query);
+	$statement->bindValue(':selected', $selected);
+	$result = $statement->execute();
 
-	// if ($result)
-	// {
-   //    header("Location: manage-projects.php");
-   //    die();
-	// }
+	if ($result)
+	{
+      header("Location: manage-projects.php");
+      die();
+	}
 }
 ?>
